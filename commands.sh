7bb36d17303165854919c920b64313ae03ba9dbc
EC2 Default User <ec2-user@ip-10-146-75-29.ap-northeast-1.compute.internal>
2012-03-04 13:17:33 +0000

    install mongodb

--- commands.sh
+++ commands.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# working on AMI Linux image
+# working on AMI Linux image x86_64
 
 # enable epel repo
 yum install patch
@@ -14,5 +14,10 @@ cp .tmux.conf ~/.tmux.conf
 
 # install nginx
 yum install nginx
+
+# install mongodb
+# error occurred when I try to install from epel repo
+cp 10gen.repo /etc/yum.repos.d/10gen.repo
+yum install mongo-10gen mongo-10gen-server
 
 
