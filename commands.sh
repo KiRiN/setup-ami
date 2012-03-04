7b6143d97c1fc545a9d535197f006a53ab1f5ad3
EC2 Default User <ec2-user@ip-10-146-75-29.ap-northeast-1.compute.internal>
2012-03-04 12:44:22 +0000

    add tmux setting

--- commands.sh
+++ commands.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 # working on AMI Linux image
 
-yum install git
-yum install patch
-
 # enable epel repo
+yum install patch
 patch /etc/yum.repos.d/epel.repo epel.repo.patch
+
+# install tools
+yum install tmux
+yum install git
+
+# settings
+cp .tmux.conf ~/.tmux.conf
+
+# install nginx
+yum install nginx
 
 
