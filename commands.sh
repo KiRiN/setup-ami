fccc31590ac3f5d08fecfe22f7f86c51b2858416
Matsushima Hiroki <ec2-user@ip-10-156-203-161.ap-northeast-1.compute.internal>
2012-03-20 08:09:09 +0000

    small update

--- commands.sh
+++ commands.sh
@@ -2,31 +2,36 @@
 # working on AMI Linux image x86_64
 
 # enable epel repo
-yum install patch
+sudo yum install patch
 patch /etc/yum.repos.d/epel.repo epel.repo.patch
 
 # install tools
-yum install tmux
-yum install git
+sudo yum install tmux
+sudo yum install git
+
+# git settings
+mkdir ~/git
+git config --global user.name "Matsushima Hiroki"
+git config --global user.mailaddress "matsushima.hiroki@mail.address.dummy"
 
 # settings
 cp .tmux.conf ~/.tmux.conf
 
 # install nginx
-yum install nginx
+sudo yum install nginx
 
 # install mongodb
 # error occurred when I try to install from epel repo
-cp 10gen.repo /etc/yum.repos.d/10gen.repo
-yum install mongo-10gen mongo-10gen-server
+sudo cp 10gen.repo /etc/yum.repos.d/10gen.repo
+sudo yum install mongo-10gen mongo-10gen-server
 
 # install sbt
-rpm -ivh http://rpm.typesafe.com/typesafe-repo-2.0.0-1.noarch.rpm
-yum install --nogpgcheck sbt
+sudo rpm -ivh http://rpm.typesafe.com/typesafe-repo-2.0.0-1.noarch.rpm
+sudo yum install --nogpgcheck sbt
 sbt
 
 # install scalatra
-git clone http://github.com/scalatra/scalatra-sbt-prototype.git ~/scalatra-sbt-prototype.git
+git clone http://github.com/scalatra/scalatra-sbt-prototype.git ~/git/scalatra-sbt-prototype.git
 
 
 
