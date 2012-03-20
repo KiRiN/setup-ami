6adc9e860d1ba0016bf57c802abf6017cd1654ef
EC2 Default User <ec2-user@ip-10-146-59-235.ap-northeast-1.compute.internal>
2012-03-20 07:45:50 +0000

    install scalatra

--- commands.sh
+++ commands.sh
@@ -20,4 +20,14 @@ yum install nginx
 cp 10gen.repo /etc/yum.repos.d/10gen.repo
 yum install mongo-10gen mongo-10gen-server
 
+# install sbt
+rpm -ivh http://rpm.typesafe.com/typesafe-repo-2.0.0-1.noarch.rpm
+yum install --nogpgcheck sbt
+sbt
+
+# install scalatra
+git clone http://github.com/scalatra/scalatra-sbt-prototype.git ~/scalatra-sbt-prototype.git
+
+
+
 
