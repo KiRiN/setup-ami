d8c5797cda4ce39bfe75b8f6979edb1b8318809a
Matsushima Hiroki <ec2-user@ip-10-156-183-212.ap-northeast-1.compute.internal>
2012-05-06 10:04:22 +0000

    add swapfile

--- commands.sh
+++ commands.sh
@@ -52,7 +52,12 @@ cs n8han/giter8
 ## sbt
 ### > container:start & access with http port 8080
 
-
+# make swap
+sudo dd if=/dev/zero of=/var/swapfile bs=1M count=2048 &&
+sudo chmod 600 /var/swapfile &&
+sudo mkswap /var/swapfile &&
+echo /var/swapfile none swap defaults 0 0 | sudo tee -a /etc/fstab &&
+sudo swapon -a
 
 
 
